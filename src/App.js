e3f389b87e9e8e9d846ec761a6cc1e00b12bd4c4
Sazib1996 <hmsazib21@gmail.com>
2022-04-13 01:50:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
-  return (
+  const [counter, setcounter]=useState(100);
+
+  const increase=(data)=>{
+    setcounter(counter + data)
+  }
+
+  const decrease=(data)=>{
+    setcounter(counter - data)
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The value of counter is : "{counter}"</h1>
+      <button onClick={() => increase(1)}>
+        <h4>Increase By  :  1</h4>
+      </button>
+      <button onClick={() => increase(5)}>
+        <h4>Increase By  : 5</h4>
+      </button>
+      <button onClick={() => increase(100)}>
+        <h4>Increase By  : 100</h4>
+      </button><br/>
+      <button onClick={() => decrease(1)}>
+        <h4>Decrease By  : 1</h4>
+      </button>
+      <button onClick={() => decrease(25)}>
+        <h4>Decrease By  : 25</h4>
+      </button>
+      <button onClick={() => decrease(100)}>
+        <h4>Decrease By  : 100</h4>
+      </button>
     </div>
   );
 }
